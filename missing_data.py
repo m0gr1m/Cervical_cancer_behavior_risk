60f7ad1e0a6d5f5dd6fc96544658737000824da3
Mogrim <cholerogram@outlook.com>
2021-10-17 03:27:12 +0200

    looking for missing data

--- missing_data.py
+++ missing_data.py
@@ -1,3 +1,14 @@
 import pandas as pd
+import numpy as np
 import missingno as msno
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("sobar-72.csv")
+
+# Checking the percentage of missing values
+print(np.round(df.isnull().sum()/len(df)*100, 2))
+
+# Visualization
+md_plot = msno.matrix(df, figsize=[8, 8], fontsize=10)
+md_dendrogram = msno.dendrogram(df, figsize=[8, 8], fontsize=10)
+plt.show()
