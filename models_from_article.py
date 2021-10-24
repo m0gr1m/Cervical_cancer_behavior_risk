3cff4c6ff47c5f59bb1fe7eb35d5de26bc89a641
Jakub Popławski <jakub.poplawski.pk@gmail.com>
2021-10-24 20:17:58 +0200

    Models done

--- models_from_article.py
+++ models_from_article.py
@@ -1,14 +1,49 @@
-# packages
+# packages -------------------------------------------------------
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 
 pd.set_option('display.width', 400)
 pd.set_option('display.max_columns', 18)
 
-# dataset
+# dataset --------------------------------------------------------
 df = pd.read_csv('sobar-72.csv')
 X = df.drop(columns='ca_cervix')
-y = df.loc[:,'ca_cervix'].values
+y = df.loc[:, 'ca_cervix'].values
 print('X shape: ', X.shape, '\ny shape: ', y.shape)
 
+# pre-processing and models --------------------------------------
+models = {"LR": LogisticRegression(),
+          "NB": GaussianNB()}
+
+# Lists for results stores
+names = []
+results = []
+sc = 'accuracy'
+
+# Loop through models
+for name, model in models.items():
+    pipe = Pipeline([
+        ("preProc", StandardScaler()),
+        ("model", model)
+    ])
+
+    cv_res = cross_val_score(pipe, X, y, cv=10, scoring=sc)
+    results.append(cv_res)
+    names.append(name)
+    print("%s: %f (%f)" % (name, cv_res.mean(), cv_res.std()))
+
+# boxplot algorithm comparison
+plt.rcParams['figure.figsize'] = [8, 8]
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)  # all on the same box
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.ylim(0, 1.1)
+plt.ylabel(sc.capitalize())
+plt.show()
